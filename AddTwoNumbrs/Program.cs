978c334ed223adcde7da20b011453df4f40300fa
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-06-08 19:49:28 -0300

    melhroando o algoritmo paare volujir profissionalmente do leetcode

--- AddTwoNumbrs/Program.cs
+++ AddTwoNumbrs/Program.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using AddTwoNumbrs.Control;
+using AddTwoNumbrs.Dominio;
+using System;
 
 namespace AddTwoNumbrs
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ControlaValor controlaValor = new ControlaValor();
+            Console.WriteLine("Calculo1: " + controlaValor.Listar());
+            Console.WriteLine("Calculo2: " + controlaValor.Listar1());
+
+            // Se quiser também exibir a soma:
+            Console.WriteLine("Soma total: " + controlaValor.Somar(null));
         }
     }
 }
