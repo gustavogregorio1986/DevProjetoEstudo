dce91e78601c52d5865c016437f46110b9a1866c
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-06-11 15:19:37 -0300

    Desenvoplver a logica de programação

---
+++ ReverseInteger/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ReverseInteger
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 1234;
+            int inverter = 0;
+
+            Console.WriteLine("Inversão com for");
+            for (; numero > 0; numero /= 10)
+            {
+                int digito = numero % 10;
+                inverter = inverter * 10 + digito;
+            }
+
+            Console.WriteLine("Numero invertidos com for são: " + inverter);
+
+            Console.WriteLine("Inversão com while");
+            while (numero > 0)
+            {
+                int digito = numero % 10;
+                inverter = inverter * 10 + digito;
+                numero = numero / 10;
+            }
+
+            Console.WriteLine("Numero invertidos com while são: " + inverter);
+        }
+    }
+}
---
+++ ZigzagConversion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ZigzagConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string lista = "aviao";
+            string invertida = "";
+
+            for (int i = lista.Length - 1; i >= 0; i--)
+            {
+                invertida += lista[i];
+            }
+
+            Console.WriteLine("Invertida: " + invertida);
+        }
+    }
+}
