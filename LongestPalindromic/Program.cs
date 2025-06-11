03d5cd79818d8473996336139f3a5036d44d41ef
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-06-11 12:15:10 -0300

    Desenvolvendo logica de programção mais complexas

---
+++ LongestPalindromic/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LongestPalindromic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string letra = "babad";
+
+            char[] caracteres = letra.ToCharArray();
+
+            foreach (char c in caracteres)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
---
+++ LongestPalindromic0/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace LongestPalindromic0
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "babad";
+            string resultado = LongestPalindrome(s);
+
+            Console.WriteLine("maior substring paslimic: " + resultado);
+        }
+
+        public static string LongestPalindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s) || s.Length < 1) return "";
+
+            int start = 0, end = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                int len1 = ExpandirDoCentro(s, i, i);     // Palíndromo de tamanho ímpar
+                int len2 = ExpandirDoCentro(s, i, i + 1); // Palíndromo de tamanho par
+                int len = Math.Max(len1, len2);
+
+                if (len > end - start)
+                {
+                    start = i - (len - 1) / 2;
+                    end = i + len / 2;
+                }
+            }
+
+            return s.Substring(start, end - start + 1);
+        }
+
+        private static int ExpandirDoCentro(string s, int esquerda, int direita)
+        {
+            while (esquerda >= 0 && direita < s.Length && s[esquerda] == s[direita])
+            {
+                esquerda--;
+                direita++;
+            }
+
+            return direita - esquerda - 1;
+        }
+
+    }
+}
