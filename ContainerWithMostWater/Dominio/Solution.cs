48867d915c0b652ec8cedf0df6490c22116e26e2
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-06-12 21:08:18 -0300

    Dersenvolver sistemnas com logica de programação utilizando leetcode

---
+++ ContainerWithMostWater/Dominio/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainerWithMostWater.Dominio
+{
+    public class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+            int left = 0;
+            int right = height.Length - 1;
+            int maxArea = 0;
+
+            while (left < right)
+            {
+                int width = right - left;
+                int currHeight = Math.Min(height[left], height[right]);
+                int area = width * currHeight;
+                maxArea = Math.Max(maxArea, area);
+
+                if (height[left] < height[right])
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            return maxArea;
+        }
+    }
+}
---
+++ ContainerWithMostWater/Program.cs
@@ -0,0 +1,19 @@
+﻿using ContainerWithMostWater.Dominio;
+using System;
+using System.Collections.Generic;
+
+namespace ContainerWithMostWater
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] height = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            Solution solution = new Solution();
+            Console.WriteLine(solution.MaxArea(height));  // Output: 49
+
+            int[] height2 = new int[] { 1, 1 };
+            Console.WriteLine(solution.MaxArea(height2)); 
+        }
+    }
+}
